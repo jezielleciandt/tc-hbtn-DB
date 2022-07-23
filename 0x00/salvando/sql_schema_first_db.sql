2e11ab2b2e92b3557b73c659ab4e168a524efc59
root <root@lnb036055cps.ciandt.global>
2022-07-22 22:35:55 -0300

    4. Salvando o SQL Schema - SQLite

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE Cliente( id integer primary key autoincrement, nome varchar(80) not null, email varchar(40) not null, telefone integer);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
